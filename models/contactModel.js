b2f55021b3a46ac021237efb6bc6824b92702170
mondalraj <mondal021002rajib@gmail.com>
2023-05-23 13:39:54 +0530

    all crud apis created

---
+++ models/contactModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const contactSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required."],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+    },
+    phone: {
+      type: String,
+      required: [true, "Phone is required."],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Contact = mongoose.model("Contact", contactSchema);
+
+module.exports = Contact;
