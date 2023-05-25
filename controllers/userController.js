b8a82f76cd35e61b531e2664f06e83753ed6fe01
mondalraj <mondal021002rajib@gmail.com>
2023-05-25 12:40:59 +0530

    jwt access_token generated on login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,85 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //@desc Register a new user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register a new user" });
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory.");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already registered.");
+  }
+
+  // Hash Password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data.");
+  }
 });
 
 //@desc Login user
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "login user" });
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory.");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(400);
+    throw new Error("Invalid email.");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    res.status(400);
+    throw new Error("Invalid password.");
+  }
+
+  const accessToken = jwt.sign(
+    {
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "1m" }
+  );
+
+  res.status(200).json({ accessToken });
 });
 
 //@desc Current user info
--- models/userModel.js
+++ models/userModel.js
@@ -21,6 +21,6 @@ const userSchema = mongoose.Schema(
   }
 );
 
-const User = mongoose.model("User", contactSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
